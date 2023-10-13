ad4ad6112c9a22e499fe3e8bc9caea9513402aab
James Harton <james@harton.nz>
2023-10-13 18:46:21 +1300

    chore: empty OTP app.

---
+++ lib/switch.ex
@@ -0,0 +1,18 @@
+defmodule Switch do
+  @moduledoc """
+  Documentation for `Switch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Switch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
