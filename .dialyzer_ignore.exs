90c60759ca1c71d318b84037def9874fd0d424d7
James Harton <james@harton.nz>
2024-06-21 19:28:37 +1200

    chore: ignore a dialyzer warning I just can't wrap my head around.

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,6 @@
+[
+  {"lib/wayfarer/server/plug.ex", :call},
+  {"lib/wayfarer/server/proxy.ex", :invalid_contract},
+  {"test/support/http_request.ex", :call},
+  {"test/support/http_request.ex", :unused_fun}
+]
