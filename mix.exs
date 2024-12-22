7fb370dd07c39750324fd9e0cdda5eec291059cf
James Harton <james@harton.nz>
2024-12-22 15:25:35 +1300

    chore: Add igniter as an optional dependency.

--- mix.exs
+++ mix.exs
@@ -113,6 +113,7 @@ defmodule Wayfarer.MixProject do
       {:ex_doc, ">= 0.0.0", opts},
       {:faker, "~> 0.18", opts},
       {:git_ops, "~> 2.6", opts},
+      {:igniter, "~> 0.5", Keyword.put(opts, :optional, true)},
       {:mimic, "~> 1.7", Keyword.delete(opts, :runtime)},
       {:mix_audit, "~> 2.1", opts}
     ]
