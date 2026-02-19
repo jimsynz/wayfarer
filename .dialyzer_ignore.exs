1ab9dae97728508a29527cd85acb80682c03191e
James Harton <james@harton.nz>
2026-02-19 15:45:09 +1300

    fix: add websocket_proxy dialyzer ignores for MIX_ENV=test
    Dialyzer under MIX_ENV=test infers that WebSocket.new/4 can never
    return {:ok, _, _}, flagging pattern_match and unused_fun errors.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,7 @@
 [
   {"lib/wayfarer/server/proxy.ex", :unknown_function},
+  {"lib/wayfarer/server/websocket_proxy.ex", :pattern_match},
+  {"lib/wayfarer/server/websocket_proxy.ex", :unused_fun},
   {"lib/wayfarer/target/check.ex", :unknown_function},
   {"test/support/http_request.ex", :unknown_function}
 ]
